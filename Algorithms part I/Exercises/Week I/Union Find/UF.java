29c13ed2d676aaf05d4afb050f3f48370f715b8e
Rifat <mrifat.hammad@gmail.com>
2013-02-05 23:09:45 +0200

    Fixed My Union Find

--- Algorithms part I/Exercises/Week I/Union Find/UF.java
+++ Algorithms part I/Exercises/Week I/Union Find/UF.java
@@ -1,27 +1,70 @@
-import java.util.list;
+import java.util.List;
 import java.util.ArrayList;
 public class UF{
-
+private List<int[]> component_arrays;
   public UF(int N){
-    List<int[]> component_arrays = new ArrayList<int[]>(N);
+    component_arrays = new ArrayList<int[]>(N);
     fill_arrays(N);
   }
-
-  void fill_array(int N){
-    for(int i=0; i<N; i++){
-      component_arrays[i][0] = i;
-    }
-
+  
+  private void fill_arrays(int n) {
+	  int[] x;
+	  for(int i=0; i<n; i++){
+		  component_arrays.add(new int[1]);
+		  x = component_arrays.get(i);
+		  for(int j=0; j<1;j++){
+			  x[j] = i;
+		  }
+		  
+	  }
+	  
   }
-
+  
   void union(int p, int q){
-    int q_index;
-    if(!is_connected()){
-      if(component_arrays[p].contains(p)){
-        component_arrays[p].add(component_arrays[component_arrays.indexOf(q)]);
-        component_arrays.remove(component_arrays.indexOf(q));
-        component_arrays.trimToSize();
-      }
-    }
+	  if(!is_connected(p, q)){
+		 int p_index = find(p);
+		 int q_index = find(q);
+		 if(p_index != -1 && q_index != -1){
+			 int[] p_array = component_arrays.get(p_index);
+			 int[] q_array = component_arrays.get(q_index);
+			 int[] p_q_array = new int[p_array.length + q_array.length];
+			 System.arraycopy(p_array, 0, p_q_array, 0, p_array.length);
+			 System.arraycopy(q_array, 0, p_q_array, p_array.length, q_array.length);
+			 component_arrays.set(p_index, p_q_array);
+			 component_arrays.remove(q_index);
+			 System.out.println(p + " " + q);
+			 System.out.println("Number of Connected components is: " + component_arrays.size());
+			 
+			 
+		 }
+		 
+	  }
+	  
   }
+  private int find(int component) {
+	  int[] x;
+	  for(int i = 0; i<component_arrays.size(); i++){
+		  x = component_arrays.get(i);
+		  for(int j = 0; j<x.length; j++){
+			  if(x[j] == component){
+				  return i;
+			  }
+			  
+		  }
+		  
+	  }
+	  
+	  return -1;
 }
+  
+  private boolean is_connected(int p, int q) {
+	  if(find(p) == find(q)){
+		  return true;
+	  }
+	
+	  return false;
+  }
+  
+}
+
+
