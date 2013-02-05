01849cce16ffe541d8ca12410ca89d4620f42c3d
Rifat <mrifat.hammad@gmail.com>
2013-02-05 23:16:41 +0200

    Fixed indentations

--- Algorithms part I/Exercises/Week I/Union Find/Main.java
+++ Algorithms part I/Exercises/Week I/Union Find/Main.java
@@ -1,18 +1,18 @@
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		UF uf = new UF(10);
-		uf.union(2, 1);
-		uf.union(1, 5);
-		uf.union(0, 1);
-		uf.union(4, 2);
-		uf.union(1, 6);
-		uf.union(7, 9);
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    UF uf = new UF(10);
+    uf.union(2, 1);
+    uf.union(1, 5);
+    uf.union(0, 1);
+    uf.union(4, 2);
+    uf.union(1, 6);
+    uf.union(7, 9);
 
-	}
+  }
 
 }
--- Algorithms part I/Exercises/Week I/Union Find/UF.java
+++ Algorithms part I/Exercises/Week I/Union Find/UF.java
@@ -1,70 +1,70 @@
 import java.util.List;
 import java.util.ArrayList;
 public class UF{
-private List<int[]> component_arrays;
+  private List<int[]> component_arrays;
   public UF(int N){
     component_arrays = new ArrayList<int[]>(N);
     fill_arrays(N);
   }
-  
+
   private void fill_arrays(int n) {
-	  int[] x;
-	  for(int i=0; i<n; i++){
-		  component_arrays.add(new int[1]);
-		  x = component_arrays.get(i);
-		  for(int j=0; j<1;j++){
-			  x[j] = i;
-		  }
-		  
-	  }
-	  
+    int[] x;
+    for(int i=0; i<n; i++){
+      component_arrays.add(new int[1]);
+      x = component_arrays.get(i);
+      for(int j=0; j<1;j++){
+        x[j] = i;
+      }
+
+    }
+
   }
-  
+
   void union(int p, int q){
-	  if(!is_connected(p, q)){
-		 int p_index = find(p);
-		 int q_index = find(q);
-		 if(p_index != -1 && q_index != -1){
-			 int[] p_array = component_arrays.get(p_index);
-			 int[] q_array = component_arrays.get(q_index);
-			 int[] p_q_array = new int[p_array.length + q_array.length];
-			 System.arraycopy(p_array, 0, p_q_array, 0, p_array.length);
-			 System.arraycopy(q_array, 0, p_q_array, p_array.length, q_array.length);
-			 component_arrays.set(p_index, p_q_array);
-			 component_arrays.remove(q_index);
-			 System.out.println(p + " " + q);
-			 System.out.println("Number of Connected components is: " + component_arrays.size());
-			 
-			 
-		 }
-		 
-	  }
-	  
+    if(!is_connected(p, q)){
+      int p_index = find(p);
+      int q_index = find(q);
+      if(p_index != -1 && q_index != -1){
+        int[] p_array = component_arrays.get(p_index);
+        int[] q_array = component_arrays.get(q_index);
+        int[] p_q_array = new int[p_array.length + q_array.length];
+        System.arraycopy(p_array, 0, p_q_array, 0, p_array.length);
+        System.arraycopy(q_array, 0, p_q_array, p_array.length, q_array.length);
+        component_arrays.set(p_index, p_q_array);
+        component_arrays.remove(q_index);
+        System.out.println(p + " " + q);
+        System.out.println("Number of Connected components is: " + component_arrays.size());
+
+
+      }
+
+    }
+
   }
   private int find(int component) {
-	  int[] x;
-	  for(int i = 0; i<component_arrays.size(); i++){
-		  x = component_arrays.get(i);
-		  for(int j = 0; j<x.length; j++){
-			  if(x[j] == component){
-				  return i;
-			  }
-			  
-		  }
-		  
-	  }
-	  
-	  return -1;
-}
-  
-  private boolean is_connected(int p, int q) {
-	  if(find(p) == find(q)){
-		  return true;
-	  }
-	
-	  return false;
+    int[] x;
+    for(int i = 0; i<component_arrays.size(); i++){
+      x = component_arrays.get(i);
+      for(int j = 0; j<x.length; j++){
+        if(x[j] == component){
+          return i;
+        }
+
+      }
+
+    }
+
+    return -1;
   }
-  
+
+  private boolean is_connected(int p, int q) {
+    if(find(p) == find(q)){
+      return true;
+    }
+
+    return false;
+  }
+
 }
 
 
